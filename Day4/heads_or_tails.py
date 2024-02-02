e552e1a94ebbb573b67c2afd373cb5225520a7ba
RowanPl <rowanplooij@hotmail.com>
2024-02-02 21:05:21 +0100

    no day6 as it was an online thing

--- Day4/heads_or_tails.py
+++ Day4/heads_or_tails.py
@@ -5,10 +5,14 @@ head_tails = "Heads"
 if random_int >= 5:
     head_tails = "Tails"
 
+heads_or_tail = input("Guess Heads or Tails: ").lower().capitalize()
 
-heads_or_Tail = input("Guess Heads or Tails")
-if heads_or_Tail == "Heads" and heads_or_Tail == "Heads":
+if heads_or_tail == "Heads" and head_tails == "Heads":
     print("Heads")
     print("You win!")
+elif heads_or_tail == "Tails" and head_tails == "Tails":
+    print("Tails")
+    print("You win")
 else:
-
+    print("You lose")
+    print(f"It was {head_tails}")
