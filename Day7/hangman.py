d8e5b1ba581e4285ee19f57023b3cd63afc75f1a
RowanPl <rowanplooij@hotmail.com>
2024-02-03 06:28:35 +0100

    finished hangman

--- Day7/hangman.py
+++ Day7/hangman.py
@@ -1,31 +1,49 @@
 import random
+import hangman_acii
+import hangman_words
 
-word_list = ["python", "ruby", "javascript", "java", "springboot"]
-
+stages = hangman_acii.stages
+logo = hangman_acii.logo
 end_of_game = False
-
-word = random.choice(word_list)
 win = False
-lives = 10
-# print(word)
-
+lives = 6
+word = random.choice(hangman_words.word_list)
 display = []
+guessed_letters = []
+
+print(logo)
+
 for _ in range(len(word)):
     display.append("_")
 
 
+def check():
+    if "_" not in display:
+        print("You win")
+        return True
+    if lives == 0:
+        print("You lose")
+        print("the word was: " + word)
+        return True
+
+
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
+
+    if guess in guessed_letters:
+        print("You already guessed that letter.")
+        continue
+
+    guessed_letters.append(guess)
     for position in range(len(word)):
         letter = word[position]
         if letter == guess:
             display[position] = letter
-        else:
-            lives -= 1
+
+    if guess not in word:
+        print(guess + " was not in the word.")
+        lives = lives - 1
+        print(stages[lives])
 
     print(display)
-
-    if "_" not in display:
-        end_of_game = True
-
-print("You Win")
+    end_of_game = check()
